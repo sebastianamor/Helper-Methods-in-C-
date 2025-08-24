7a788b926140d7970a73f71c69f3771e885bc10e
sebastian lapt <smoreira11@hotmail.com>
2025-08-25 08:08:42 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -40,33 +40,40 @@
   //  Console.WriteLine($"-- {pallet}");
 //}
 
-string[] pallets = [ "B14", "A11", "B12", "A13" ];
-Console.WriteLine("");
+//string[] pallets = [ "B14", "A11", "B12", "A13" ];
+//Console.WriteLine("");
 
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+//Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+//Console.WriteLine("");
+//Array.Resize(ref pallets, 6);
+//Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
 
-pallets[4] = "C01";
-pallets[5] = "C02";
+//pallets[4] = "C01";
+//pallets[5] = "C02";
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+//Console.WriteLine("");
+//Array.Resize(ref pallets, 3);
+//Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
+
