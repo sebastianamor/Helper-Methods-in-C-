94ff4a5c43b91a5828b052a44d3636049a77c7c2
sebastian lapt <smoreira11@hotmail.com>
2025-09-03 11:58:04 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -140,13 +140,4 @@
 
 
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-char[] valueArray = orderStream.ToCharArray();
-string result = new string(valueArray);
-string result = String.Join( valueArray);
-Console.WriteLine(result);
 
-string[] items = result.Split('');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
