bc55f142547d04556cd46b480cf27840e40ac892
sebastian lapt <smoreira11@hotmail.com>
2025-09-04 12:06:16 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -138,6 +138,18 @@
 
 // el resultado es ehT kciuq nworb xof spmuj revo eht yzal god
 
-
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
 
+foreach (var item in items)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
